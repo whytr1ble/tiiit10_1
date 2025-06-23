2ad8fe9ba288177b0a952662df9ee315a9f226ca
whytr1ble <tr1bleeeee@gmail.com>
2025-06-23 13:54:03 +0300

    Исправлены ошибки в программе

--- root.cpp
+++ root.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cmath>    // For sqrt() and pow() functions
 #include <iomanip>  // For setprecision()
 
@@ -8,25 +8,27 @@ int main() {
     double a, b, c;
     double discriminant;
     double root1, root2;
-    
+
     cout << "Enter coefficients a, b and c: ";
-    cin >> a >> b >> c
-    
-    discriminant = b*b - 4*a*c;
-    
-    if (dscriminant > 0) {
-        root1 = (-b + sqrt(discriminant)) / (2*a);
-        root2 = (-b - sqrt(discriminant)) / (2*a);
+    cin >> a >> b >> c;  // ← добавлена точка с запятой
+
+    discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0) {  // ← исправлена опечатка
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
         cout << fixed << setprecision(4);
         cout << "Root 1 = " << root1 << endl;
         cout << "Root 2 = " << root2 << endl;
     }
     else if (discriminant == 0) {
-        root1 = -b/(2*a);
+        root1 = -b / (2 * a);
         cout << fixed << setprecision(4);
         cout << "Root = " << root1 << endl;
     }
     else {
         cout << "No real roots." << endl;
     }
+
+    return 0;
 }
